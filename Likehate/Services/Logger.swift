213cb9860fdb0e02dfb88969c950070e9d28a161
ojun9 <eu.bkw7jdcpa2r@gmail.com>
2026-06-06 01:11:57 +0900

    Remove unused logger categories

--- Likehate/Services/Logger.swift
+++ Likehate/Services/Logger.swift
@@ -4,8 +4,6 @@ import os
 enum Logger: Sendable {
   static let standard: os.Logger = makeLogger(category: "Standard")
   static let analytics: os.Logger = makeLogger(category: "Analytics")
-  static let firestore: os.Logger = makeLogger(category: "Firestore")
-  static let notification: os.Logger = makeLogger(category: "Notification")
 
   static func makeLogger(category: String) -> os.Logger {
     .init(
